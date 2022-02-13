a69af8a1d0e6fd9c47809bb0f2e258fe61a6a13c
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-13 13:20:03 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from "react-router-dom";
+import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
