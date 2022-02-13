8e55ed5ca5b4a09b0a130ff44990aaf1c898a77f
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-12 20:44:54 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
