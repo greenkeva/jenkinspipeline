a01899fce515164db46335326a1aab7ee224bd11
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-12 21:10:51 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from "react-router-dom";
 import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders daily quotes', () => {
+  const { getByText } = render(<App />);
+  const text = getByText("quotes");
+  expect(text).toBeInTheDocument();
+});
 
-it("navigates home when you click the logo", () => {
-
-  // Render app
-  render(
-    <MemoryRouter initialEntries={['/music']}>
-      <App />
-    </MemoryRouter>,
-    root
-   });
