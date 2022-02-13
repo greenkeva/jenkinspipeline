afc930d2dc8c722539be1b11d54573ffc72fbbef
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-13 13:16:05 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { MemoryRouter } from "react-router-dom";
 import App from './App';
 
-test('renders daily quotes', () => {
-  const { getByText } = render(<App />);
-  const text = screen.getByText("quotes");
-  expect(text).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
 
