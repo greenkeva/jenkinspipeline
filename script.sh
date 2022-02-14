047ef6b9e6e42def042d289487f7cd5f04dfcf13
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-13 19:18:51 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
                         nvm install 14.17.6                        
-                        npm --version
-                        npm install
-                        npm install enzyme
-                        npm install --save-dev @wojtekmaj/enzyme-adapter-react-17
-                        npm run build
+                        sh 'npm --version'
+                        sh 'npm install'
+                        sh 'npm install enzyme'
+                        sh 'npm install --save-dev @wojtekmaj/enzyme-adapter-react-17'
+                        sh 'npm run build'
                         cf api https://api.ng.bluemix.net
                         cf login -u $IBM_CLOUD_DEVOPS_CREDS_USR -p $IBM_CLOUD_DEVOPS_CREDS_PSW -o $IBM_CLOUD_DEVOPS_ORG -s staging
                         echo "Deploying...."
                         export CF_APP_NAME="staging-$IBM_CLOUD_DEVOPS_APP_NAME"
                         cf delete $CF_APP_NAME -f
                         cf push $CF_APP_NAME -n $CF_APP_NAME -m 64M -i 1
-                        # use "cf icd --create-connection" to enable traceability
+#                          use "cf icd --create-connection" to enable traceability
                         cf icd --create-connection $IBM_CLOUD_DEVOPS_WEBHOOK_URL $CF_APP_NAME
                         export APP_URL=http://$(cf app $CF_APP_NAME | grep urls: | awk '{print $2}')
