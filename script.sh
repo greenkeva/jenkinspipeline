801b18e4afe0e090b69dfeaec56c5cf34e01fe65
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-13 18:21:10 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+                        cf api https://api.ng.bluemix.net
+                        cf login -u $IBM_CLOUD_DEVOPS_CREDS_USR -p $IBM_CLOUD_DEVOPS_CREDS_PSW -o $IBM_CLOUD_DEVOPS_ORG -s staging
+                        echo "Deploying...."
+                        export CF_APP_NAME="staging-$IBM_CLOUD_DEVOPS_APP_NAME"
+                        cf delete $CF_APP_NAME -f
+                        cf push $CF_APP_NAME -n $CF_APP_NAME -m 64M -i 1
+                        # use "cf icd --create-connection" to enable traceability
+                        cf icd --create-connection $IBM_CLOUD_DEVOPS_WEBHOOK_URL $CF_APP_NAME
+                        export APP_URL=http://$(cf app $CF_APP_NAME | grep urls: | awk '{print $2}')
