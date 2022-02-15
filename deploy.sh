d6b47c14e5ef5f81085cb32bf5f11b7f219f9697
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-15 13:56:44 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ set -e
 
 echo "Installing the IBM Cloud CLI"
 
-wget https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/1.2.3/IBM_Cloud_CLI_1.2.3_386.tar.gz
-tar -xvf IBM_Cloud_CLI_1.2.3_386.tar.gz
-./Bluemix_CLI/install_bluemix_cli
+wget 'https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/1.2.3/IBM_Cloud_CLI_1.2.3_386.tar.gz'
+tar -xvf 'IBM_Cloud_CLI_1.2.3_386.tar.gz'
+'./Bluemix_CLI/install_bluemix_cli'
 
 
 # Ignore updates because they need confirmation from the user
