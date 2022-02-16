706ee44c31c539014c818601e1cbf7d0b890efff
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-16 11:04:48 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 echo "Deploying...."
 
-if [ -f ~/.ibmcloud.rc ]; then
-    source ~/.ibmcloud.rc
-fi
-
-
 ibmcloud login --apikey "$IBM_CLOUD_DEVOPS_API_KEY" -a https://api.ng.bluemix.net
 ibmcloud target --cf
 cf login -a https://api.us-south.cf.cloud.ibm.com -u apikey $IBM_CLOUD_DEVOPS_API_KEY -o $IBM_CLOUD_DEVOPS_ORG -s dev
