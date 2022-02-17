1e2ad4ac4326682cbe0a9fad073eb6b79d1191f0
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-16 19:09:36 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ npm install
 npm install enzyme
 npm install --save-dev @wojtekmaj/enzyme-adapter-react-17
 npm run build
-npm i -g @ibmgaragecloud/cloud-native-toolkit-cli
-npm i @marcosbv/ibmcloud-apikey-single-auth
