d922a06501ce2b5303cde46cb92b4e6406b54259
greenkeva <85896563+greenkeva@users.noreply.github.com>
2022-02-23 18:56:42 -0600

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
         return (
         <div id="div">
         <div id="quotes">
-            <h2>Random Quote for Today </h2>
+            <h2>Daily Quote</h2>
         </div>
         <div id="quote">
             <h2>{error}</h2>
